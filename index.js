80d2fc8701445f77d9cdb88c11d506e29109b258
Stas Siuris <stassiuris@gmail.com>
2023-06-08 20:38:42 +0300

    solve task

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 require('dotenv').config();
+const bodyParser = require("body-parser");
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const dns = require("dns")
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+let urlMem = [];
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+(function () {
+    setTimeout(() => {
+        urlMem = []
+    }, 1000000)
+})()
+
+app.post('/api/shorturl', (req, res) => {
+    const url = req.body.url
+    dns.lookup(`${url}`, (err, address) => {
+        if (err) {
+            return res.json({error: 'invalid url'})
+        }
+        urlMem.push(url)
+        return res.json({
+            original_url: url,
+            short_url: urlMem.length
+        })
+    })
+
+})
+
+app.get('/api/shorturl/:id', (req, res) => {
+    const id = req.params.id
+    const url = urlMem[id - 1]
+    if (!url) return res.end()
+    return res.redirect(url)
+})
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
 });
